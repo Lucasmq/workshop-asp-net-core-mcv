82fa6217a6b2890be873138d2302f80646dc712c
Lucas Queiroz <lucasmedeirosq@gmail.com>
2019-03-13 16:25:50 -0300

    Seeding Service

--- WebApplication1/Startup.cs
+++ WebApplication1/Startup.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using WebApplication1.Models;
+using WebApplication1.Data;
 
 namespace WebApplication1
 {
@@ -39,14 +40,17 @@ namespace WebApplication1
             services.AddDbContext<WebApplication1Context>(options =>
                 options.UseMySql(Configuration.GetConnectionString("WebApplication1Context"), builder =>
                     builder.MigrationsAssembly("WebApplication1")));
+
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
