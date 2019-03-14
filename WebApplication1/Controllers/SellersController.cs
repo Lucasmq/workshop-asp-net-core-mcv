35f9e18d2ec588a86ea68dd38ec06b7a137b6cf3
Lucas Queiroz <lucasmedeirosq@gmail.com>
2019-03-13 21:10:31 -0300

    Delete Seller

--- WebApplication1/Controllers/SellersController.cs
+++ WebApplication1/Controllers/SellersController.cs
@@ -35,11 +35,35 @@ namespace WebApplication1.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Seller seller)
+        public IActionResult Create(Seller seller) // ele já sabe pelo DepartmentId que precisa instanciar o Departament dele
         {
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value); // usa o .value pois o id é opcional e pode ser null
+            if( obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerService.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
+
 
     }
 }
